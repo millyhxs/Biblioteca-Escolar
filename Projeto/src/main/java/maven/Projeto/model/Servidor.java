b631505c0248499be821edbe215ce95d82826df2
Millena <millyhxs@gmail.com>
2025-06-24 22:20:56 -0300

    Create Servidor.java

---
+++ Projeto/src/main/java/maven/Projeto/model/Servidor.java
@@ -0,0 +1,14 @@
+package maven.Projeto.model;
+
+public class Servidor extends Usuario {
+
+    public Servidor() {
+        super();
+        super.setTipoDeUsuario("Servidor");
+    }
+
+    public Servidor(String nome, String matricula, String telefone, String email) {
+        super(nome, matricula, telefone, email);
+        super.setTipoDeUsuario("Servidor");
+    }
+}
