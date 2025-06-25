609a7d4bec03651626a53202136400b4f4c13d80
Millena <millyhxs@gmail.com>
2025-06-24 22:17:26 -0300

    Update Usuario.java

--- Projeto/src/main/java/maven/Projeto/model/Usuario.java
+++ Projeto/src/main/java/maven/Projeto/model/Usuario.java
@@ -9,4 +9,17 @@ public class Usuario {
 	protected String tipoDeUsuario;
 	protected String telefone;
 	protected String email;
+	
+        // construtor padrão
+	
+        public Usuario() {}
+
+        // construtor parametrizado
+        public Usuario(String nome, String matricula, String telefone, String email) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.telefone = telefone;
+        this.email = email;
+    }
+	
 }
