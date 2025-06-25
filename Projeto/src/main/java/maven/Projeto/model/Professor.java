174513f5ca2888e4eb285a8e134fe1299370dffd
Millena <millyhxs@gmail.com>
2025-06-24 22:20:14 -0300

    Create Professor.java

---
+++ Projeto/src/main/java/maven/Projeto/model/Professor.java
@@ -0,0 +1,14 @@
+package maven.Projeto.model;
+
+public class Professor extends Usuario {
+
+    public Professor() {
+        super();
+        super.setTipoDeUsuario("Professor");
+    }
+
+    public Professor(String nome, String matricula, String telefone, String email) {
+        super(nome, matricula, telefone, email);
+        super.setTipoDeUsuario("Professor");
+    }
+}
