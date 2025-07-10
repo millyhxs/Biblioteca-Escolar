1c3432b3247312c027b961c020c70d4406627ac5
Millena <millyhxs@gmail.com>
2025-07-10 13:22:34 -0300

    getters e setters de usuario

--- Projeto/src/main/java/maven/Projeto/model/Usuario.java
+++ Projeto/src/main/java/maven/Projeto/model/Usuario.java
@@ -22,10 +22,47 @@ public class Usuario {
         this.matricula = matricula;
         this.telefone = telefone;
         this.email = email;
-    }
+        }
         
-	public void setTipoDeUsuario(String tipoDeUsuario) {
+        
+        public String getNome() {
+    		return nome;
+    	}
+
+    	public void setNome(String nome) {
+    		this.nome = nome;
+    	}
+
+    	public String getMatricula() {
+    		return matricula;
+    	}
+
+    	public void setMatricula(String matricula) {
+    		this.matricula = matricula;
+    	}
+
+    	public String getTelefone() {
+    		return telefone;
+    	}
+
+    	public void setTelefone(String telefone) {
+    		this.telefone = telefone;
+    	}
+
+    	public String getEmail() {
+    		return email;
+    	}
+
+    	public void setEmail(String email) {
+    		this.email = email;
+    	}
+
+    	public String getTipoDeUsuario() {
+    		return tipoDeUsuario;
+    	}
+
+    	public void setTipoDeUsuario(String tipoDeUsuario) {
 		this.tipoDeUsuario = tipoDeUsuario;
-	}
+    	}
 	
-}
+	}
